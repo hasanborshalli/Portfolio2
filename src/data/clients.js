b886cb76af830abb68c264aaafc13e98f6bf5488
Hasan Borshalli <hasanborshalli@gmail.com>
2026-03-16 23:00:39 +0200

    add images

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,7 +1,7 @@
 const clients = [
-    { id: 1, image: "/images/client1.png", name: "Client 1" },
-    { id: 2, image: "/images/client2.png", name: "Client 2" },
-    { id: 3, image: "/images/client3.png", name: "Client 3" },
+    { id: 1, image: "/images/client1.jpg", name: "Client 1" },
+    { id: 2, image: "/images/client2.jpg", name: "Client 2" },
+    { id: 3, image: "/images/client3.jpg", name: "Client 3" },
 ];
 
 export default clients;
